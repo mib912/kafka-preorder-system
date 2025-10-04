4ade508b9f78fbd6f90a18564fcbe2d5715d2618
강민정 <minjung@MJs-MacBook-Air.local>
2025-10-04 18:20:05 +0900

    init: spring boot kafka producer project

---
+++ src/main/java/com/example/kafka_preorder_producer/KafkaPreorderProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_preorder_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPreorderProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPreorderProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafka_preorder_producer/KafkaPreorderProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_preorder_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPreorderProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
